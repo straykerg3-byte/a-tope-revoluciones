06313551da13e137bb73fba3c65033c8373e6891
IT COLOMBIA <APRENDICESIT@convertia.local>
2026-05-08 14:41:36 -0500

    Addición de features

---
+++ djblakee/src/main/java/com/example/djblakee/DjblakeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.djblakee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjblakeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjblakeeApplication.class, args);
+	}
+
+}
---
+++ djblakee/src/test/java/com/example/djblakee/DjblakeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.djblakee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjblakeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
